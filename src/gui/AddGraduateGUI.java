f051dd5d21e6b0aa13c483c6174643ba02637cd8
jabojohnigan <jabojohnigan93@yahoo.com>
2016-12-03 19:08:14 -0800

    Sat Dec 3: 7:01 PM Working.....

--- src/gui/AddGraduateGUI.java
+++ src/gui/AddGraduateGUI.java
@@ -1,7 +1,25 @@
 package gui;
 
+import javax.swing.JPanel;
+import java.awt.event.*;
+import javax.swing.*;
 /**
  * Created by noxor on 12/2/16.
+ * @author Jabo Johnigan
+ * @versioin 12/2/16
  */
-public class AddGraduateGUI {
+public class AddGraduateGUI extends JPanel implements ActionListener {
+    /**
+     * labels for the addGrad panel.
+     */
+    private JLabel[] lblAddGrad = new JLabel[6];
+
+    private JPanel pnlAddGrad;
+
+
+
+    public void actionPerformed(ActionEvent someAction){
+
+    }
+
 }
--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -6,16 +6,30 @@ import graduate.GraduateCollection;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.*;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JTextPane;
 
-public class GUI {
+public class GUI extends JFrame {
 
-	private JFrame frmGradTrackr;
+	/**A DeFAULT PANEL SIZE FOR JPANELS. */
+	private static final Dimension DEFAULT_F_MIN = new Dimension(175, 352);
+
+	private JFrame frameOfGradTrackr = this;
+
+	private JPanel btnPanel;
+
+	private JButton btnAddGrad, btnRunReport, btnListGrad;
 
 	/**
 	 * Launch the application.
@@ -26,8 +40,8 @@ public class GUI {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					GUI window = new GUI();
-					window.frmGradTrackr.setVisible(true);
+					GUI guiRUNOBJ = new GUI();
+					guiRUNOBJ.frameOfGradTrackr.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -39,6 +53,12 @@ public class GUI {
 	 * Create the application.
 	 */
 	public GUI() {
+		frameOfGradTrackr = new JFrame();
+		frameOfGradTrackr.setTitle("Grad Trackr");
+		frameOfGradTrackr.setBounds(100, 100, 800, 700);
+		frameOfGradTrackr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frameOfGradTrackr.getContentPane().setLayout(null);
+
 		initialize();
 	}
 
@@ -46,12 +66,7 @@ public class GUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmGradTrackr = new JFrame();
-		frmGradTrackr.setTitle("Grad Trackr");
 		//TODO: make bigger plz
-		frmGradTrackr.setBounds(100, 100, 550, 354);
-		frmGradTrackr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmGradTrackr.getContentPane().setLayout(null);
 
 		//TODO: link the buttons the controller
 		JButton btnRequestInfo = new JButton("Request Info");
@@ -60,23 +75,25 @@ public class GUI {
 			}
 		});
 		btnRequestInfo.setBounds(48, 109, 117, 29);
-		frmGradTrackr.getContentPane().add(btnRequestInfo);
+		frameOfGradTrackr.getContentPane().add(btnRequestInfo);
 		
-		JButton btnRunReport = new JButton("Run Report");
+		btnRunReport = new JButton("Run Report");
 		btnRunReport.setBounds(48, 68, 117, 29);
-		frmGradTrackr.getContentPane().add(btnRunReport);
+		frameOfGradTrackr.getContentPane().add(btnRunReport);
 		
 		JButton btnUpdateGradInfo = new JButton("Update Grad Info");
 		btnUpdateGradInfo.setBounds(48, 150, 150, 29);
-		frmGradTrackr.getContentPane().add(btnUpdateGradInfo);
+		frameOfGradTrackr.getContentPane().add(btnUpdateGradInfo);
 		
-		JButton btnAddremoveGrad = new JButton("Add/Remove Grad");
-		btnAddremoveGrad.setBounds(48, 191, 150, 29);
-		frmGradTrackr.getContentPane().add(btnAddremoveGrad);
+		btnAddGrad = new JButton("Add/Remove Grad");
+		btnAddGrad.setBounds(48, 191, 150, 29);
+		frameOfGradTrackr.getContentPane().add(btnAddGrad);
 		
 		JTextPane txtpnIdkWhatShould = new JTextPane();
 		txtpnIdkWhatShould.setText("IDK WHAT SHOULD GO HERE");
 		txtpnIdkWhatShould.setBounds(226, 68, 265, 202);
-		frmGradTrackr.getContentPane().add(txtpnIdkWhatShould);
+		frameOfGradTrackr.getContentPane().add(txtpnIdkWhatShould);
 	}
+
+
 }
