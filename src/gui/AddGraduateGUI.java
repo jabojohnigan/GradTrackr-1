4741bc79cfc6ddd2320c37f45351cdaf5b91986a
Humza <humzam99@gmail.com>
2016-12-05 13:49:21 -0800

    adding buttons for addGrad

--- src/gui/AddGraduateGUI.java
+++ src/gui/AddGraduateGUI.java
@@ -1,6 +1,5 @@
 package gui;
 
-import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -20,24 +19,45 @@ public class AddGraduateGUI extends JPanel implements ActionListener {
      */
     private static final Dimension DEFAULT_P_MIN = new Dimension(300, 600);
 
-    /**
-     * labels Name
-     */
-    private String[] lblNames = {"Enter Name", "Enter Graduate Year", "Enter GPA",
-                                "Enter Email", "Transfer Student", "Responsive"};
+  
+    
+	private JLabel[] txfLabel = new JLabel[6];
+	private JTextField[] txfField = new JTextField[6];
 
     private JPanel pnlAddGrad, pnlAddJobs;
+    
+    private JButton btnAddGrad;
 
     private JButton addEmps, addInts, addGrad;
     public AddGraduateGUI(){
         setLayout(new BorderLayout());
         setVisible(true);
         setSize(DEFAULT_P_MIN);
-        BuildPanel();
+        setUpPanel();
     }
 
-    public void BuildPanel(){
-
+    public void setUpPanel(){
+    	pnlAddGrad = new JPanel();
+    	pnlAddGrad.setLayout(new GridLayout(5, 0));
+    	add(pnlAddGrad, BorderLayout.CENTER);
+    	
+    	String[] lblNames = {"Enter Name:", "Enter Graduate Year:", "Enter GPA:",
+                "Enter Email:", "Transfer Student:", "Responsive:"};
+		for (int i = 0; i < lblNames.length; i++) {
+			JPanel panel = new JPanel();
+			panel.setLayout(new GridLayout(1, 0));
+			txfLabel[i] = new JLabel(lblNames[i]);
+			txfField[i] = new JTextField(25);
+			panel.add(txfLabel[i]);
+			panel.add(txfField[i]);
+			pnlAddGrad.add(panel);
+		}
+		
+		JPanel panel = new JPanel();
+		btnAddGrad = new JButton("Add");
+		btnAddGrad.addActionListener(this);
+		panel.add(btnAddGrad);
+		pnlAddGrad.add(panel);
     }
     public void actionPerformed(ActionEvent someAction){
 
