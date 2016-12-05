5d1dc014c7dda1a6042cffe81abf5fac1dfe8614
Humza <humzam99@gmail.com>
2016-12-05 14:32:12 -0800

    added employer, internship field

--- src/gui/AddGraduateGUI.java
+++ src/gui/AddGraduateGUI.java
@@ -3,6 +3,8 @@ package gui;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+
+import graduate.Graduate;
 /**
  * Created by noxor on 12/2/16.
  * @author Jabo Johnigan
@@ -10,9 +12,14 @@ import javax.swing.*;
  */
 public class AddGraduateGUI extends JPanel implements ActionListener {
     /**
+	 * 
+	 */
+	private static final long serialVersionUID = -1262967559520618107L;
+
+	/**
      * labels for the addGrad panel.
      */
-    private JLabel[] lblAddGrad = new JLabel[6];
+    private JLabel[] lblAddGrad = new JLabel[8];
 
     /**
      * PANEL SIZE
@@ -21,8 +28,7 @@ public class AddGraduateGUI extends JPanel implements ActionListener {
 
   
     
-	private JLabel[] txfLabel = new JLabel[6];
-	private JTextField[] txfField = new JTextField[6];
+	private JTextField[] txfField = new JTextField[8];
 
     private JPanel pnlAddGrad, pnlAddJobs;
     
@@ -38,17 +44,17 @@ public class AddGraduateGUI extends JPanel implements ActionListener {
 
     public void setUpPanel(){
     	pnlAddGrad = new JPanel();
-    	pnlAddGrad.setLayout(new GridLayout(6, 0));
+    	pnlAddGrad.setLayout(new GridLayout(9, 0));
     	add(pnlAddGrad, BorderLayout.CENTER);
     	
     	String[] lblNames = {"Enter Name:", "Enter Graduate Year:", "Enter GPA:",
-                "Enter Email:", "Transfer Student:", "Responsive:"};
+                "Enter Email:", "Transfer Student:", "Responsive:", "Employers:", "Internships:"};
 		for (int i = 0; i < lblNames.length; i++) {
 			JPanel panel = new JPanel();
 			panel.setLayout(new GridLayout(2, 0));
-			txfLabel[i] = new JLabel(lblNames[i]);
+			lblAddGrad[i] = new JLabel(lblNames[i]);
 			txfField[i] = new JTextField(25);
-			panel.add(txfLabel[i]);
+			panel.add(lblAddGrad[i]);
 			panel.add(txfField[i]);
 			pnlAddGrad.add(panel);
 		}
@@ -59,8 +65,57 @@ public class AddGraduateGUI extends JPanel implements ActionListener {
 		panel.add(addGrad);
 		pnlAddGrad.add(panel);
     }
-    public void actionPerformed(ActionEvent someAction){
-
+    
+    public void actionPerformed(ActionEvent e){
+    	if (e.getSource() == addGrad) {
+    		if(!txfField[0].getText().equals("") && !txfField[1].getText().equals("") 
+    			&& !txfField[2].getText().equals("") && !txfField[3].getText().equals("")) {
+				performAddGrad();
+			}
+    	}
     }
+    
+    /**
+     * Adds a new Grad to the system. Name, Year, GPA, and Email are required.
+     */
+	private void performAddGrad() {
+		String name = txfField[0].getText();
+		if (name.length() == 0) {
+			JOptionPane.showMessageDialog(null, "Enter a grad name");
+			txfField[0].setFocusable(true);
+			return;
+		}
+		
+		String yearStr = txfField[1].getText();
+		if (yearStr.length() == 0) {
+			JOptionPane.showMessageDialog(null, "Enter a grad year");
+			txfField[1].setFocusable(true);
+			return;
+		} else {
+			int yearInt = Integer.parseInt(yearStr);
+		}
+		
+		double gpa = Double.parseDouble(txfField[2].getText());	
+		String email = txfField[3].getText(); 
+		String transfer = txfField[4].getText();
+		String responsive = txfField[5].getText();
+		
+		
+		Graduate graduate;
+
+		
+		String message = "Client add failed";
+//		if (GraduateCollection.add(graduate)) {
+//			message = "Client added";
+//		}
+		JOptionPane.showMessageDialog(null, message);
+
+		// Clear all text fields.
+		for (int i = 0; i < txfField.length; i++) {
+			if (txfField[i].getText().length() != 0) {
+				txfField[i].setText("");
+			}
+		}		
+	}
 
 }
